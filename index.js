85f15c41651b402f65606e97b3e26fd324a9087b
Rúben Silva <rubensilva945@gmail.com>
2018-12-29 18:22:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 const Discord = require("discord.js");
-const TOKEN = "NDQwMjcxNjc2MzcwOTc2Nzcw.DgKFOQ.o7r54oyhJaNnsPBcoF09jUPwSXg";
 const btcValue = require('btc-value');
+const TOKEN = "NDQwMjcxNjc2MzcwOTc2Nzcw.DgKFOQ.o7r54oyhJaNnsPBcoF09jUPwSXg";
 
-var bot = new Discord.Client();
+let bot = new Discord.Client();
 
-bot.on("ready", function(){
- console.log("ready");
-})
+bot.on("ready", () => console.log("Servidor Pronto"))
 
-bot.on("message", function(message){
- if(message.author.equals(bot.user)) return;
-
-if (message.content == "!btc") {
-  btcValue().then(value => {
-   btcValue.getPercentageChangeLastDay().then(percentage => {
-    var perc = percentage
-    if (perc > 0){
-        message.channel.send("O preço do  btc é " + value + " $ \nSubiu (ultimo dia): " + perc + "% :hugging:");
-       } else if (perc < 0){
-        message.channel.send("O preço do  btc é " + value + " $ \nDesceu (ultimo dia): " + perc + "% :sob:");
-       } else {
-        message.channel.send("O preço do  btc é " + value + " $ \nManteve (ultimo dia): " + perc + "% :rolling_eyes:");
-       }
-
-});
-  });
+bot.on("message", mensagem => {
+ 	// Verifica se a mensagem é do próprio bot
+	if (mensagem.author.equals(bot.user)) return
+ 
+ 	if (message.content == "!btc") {
+  		btcValue().then(value => {
+   			btcValue.getPercentageChangeLastDay().then(percentage => {
+    			let perc = percentage
+    			if (perc > 0){
+        			message.channel.send("O preço do  btc é " + value + " $ \nSubiu (ultimo dia): " + perc + "% :hugging:");
+       			} else if (perc < 0){
+        			message.channel.send("O preço do  btc é " + value + " $ \nDesceu (ultimo dia): " + perc + "% :sob:");
+       			} else {
+       				 message.channel.send("O preço do  btc é " + value + " $ \nManteve (ultimo dia): " + perc + "% :rolling_eyes:");
+       			}
+			});
+  		});
+	}
  }
-});
 
 bot.login(TOKEN);
